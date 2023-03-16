68a487b0fcfabd121095cc0914abcbadf1486611
설소윤 <soyoon.seol@emotion.co.kr>
2023-03-16 14:23:08 +0900

    #1.2 developing mainpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import {Button, Navbar, Container, Nav } from 'react-bootstrap';
+import {Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
@@ -16,6 +16,46 @@ function App() {
         </Nav>
         </Container>
       </Navbar>
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <img src={`${process.env.PUBLIC_URL}https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg`} />
+            <img src={process.env.PUBLIC_URL + 'https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg' } />
+           
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
