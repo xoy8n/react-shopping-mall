f7019a476c95b11b6b7db12cd6141873844b88f3
설소윤 <soyoon.seol@emotion.co.kr>
2023-03-17 09:52:26 +0900

    #1.3 making Card component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import {Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import prdData from './data.js';
 
 function App() {
+  //surfing이라는 컴포넌트는 App 컴포넌트 안에 있다.
+  // App의 자식인 Card 컴포넌트가 쓰고 싶으면 props 전송해주면 된다.
+  let [surfing] = useState(prdData);
 
-  let [shoes] = useState()
-  
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -23,45 +25,27 @@ function App() {
       <div className='main-bg'></div>
       <Container>
         <Row>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <img src={`${process.env.PUBLIC_URL}https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg`} width="80%"/>
-            <img src={process.env.PUBLIC_URL + 'https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg' } width="80%"/>
-           
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img src="https://cms.accuweather.com/wp-content/uploads/2018/06/surf-4.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {
+            surfing.map(function(a,i){
+              return(
+                <Card surfing={surfing[i]} i={i}></Card>
+              )
+            })
+          }
         </Row>
       </Container>
     </div>
   );
+}
+
+function Card(props){
+  return(
+    <Col sm>
+      <img src={`${process.env.PUBLIC_URL}https://cms.accuweather.com/wp-content/uploads/2018/06/surf-${props.i+1}.jpg`} width="80%"/>
+      <h4>{props.surfing.title}</h4>
+      <p>{props.surfing.price}</p>
+    </Col>
+  )
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,1 +1,27 @@
-let  a = 10;
+// let a = 10;
+// export default a;
+
+let prdData = [ //array
+    { //object
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ];
+
+  export default prdData;
